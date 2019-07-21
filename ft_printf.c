1a7cbdeeb4d1b12f9c4de77b53e2edad34238400
RenataLP <46381320+RenataLP@users.noreply.github.com>
2019-07-21 17:05:49 +0300

    Add files via upload

---
+++ ft_printf.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bkristle <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/30 16:46:11 by bkristle          #+#    #+#             */
+/*   Updated: 2019/06/30 16:46:11 by bkristle         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	char i;
+
+	if (nb == -2147483648)
+		ft_putchar('-2147483648');
+	else if (nb >= 0 && nb <= 9)
+		ft_putchar(nb + '0');
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		ft_putnbr(nb / 10);
+		ft_putchar((nb % 10) + '0');
+	}
+}
+
+//typedef union	t_f
+//{
+//	double		n;
+//	struct		s_f
+//	{
+//		unsigned long m:52;
+//		unsigned int e:11;
+//		unsigned int s:1;
+//	}			t_ma;
+//}				u_f;//для определения мантиссы,
+
+int	ft_printf(const char * restrict format, ...)
+{
+	char *traverse;
+	unsigned int i;
+	char *s;
+
+	va_list arg;
+	va_start(arg, format);
+	traverse = (char *)format;
+	while (*traverse != '\0')
+	{
+		if (*traverse != '%')
+		{
+			ft_putchar(*traverse);
+			traverse++;
+		}
+		traverse++;
+		if (*traverse == 'c')
+		{
+			i = va_arg(arg, int);
+			ft_putchar(i);
+			exit (0);
+		}
+		else if (*traverse == 'd' || *traverse == 'i')
+		{
+			i = va_arg(arg, int);
+			if (i < 0)
+			{
+				i = -i;
+				ft_putchar('-');
+			}
+			ft_putnbr(i);
+			exit (0);
+		}
+		(*traverse)++;
+	}
+	va_end(arg);
+	return (0);
+}
+
+int main()
+{
+	char a = 'p';
+	int b = 1234;
+	ft_printf("%c", a);
+	printf("%c", a);
+	ft_printf("%d", b);
+	return (0);
+}
